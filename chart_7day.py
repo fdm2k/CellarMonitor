d3e58eb77c622ef6747d8371f51be0d3a717b7bb
fdm2k <fdm_2k@hotmail.com>
2014-09-01 22:13:29 +0930

    fixed chart_7day navbar

--- chart_7day.py
+++ chart_7day.py
@@ -85,7 +85,7 @@ def printHTTPheader():
         <div class="navbar navbar-default">
           <div class="container">
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bs-navbar-collapse">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
